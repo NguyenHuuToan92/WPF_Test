ec9101f282973b35ddff255d62fd802e6d75e833
Toàn Nguyễn Hữu <toannh06@gmail.com>
2025-06-29 23:23:46 +0700

    Bài tập thực hành

---
+++ WPF_Test_Lib/Class1.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Test_Lib
+{
+
+
+    [TestFixture]
+    public class Class1
+    {
+        int age;
+        string name;
+        string email;
+
+        enum Mua { Xuan = 1, Ha = 2, Thu = 3, Dong = 4 };
+
+
+        [Test]
+
+        public void TinhCong()
+        {
+            int a = 23;
+            int b = 21;
+            int c = a%b;
+            Console.WriteLine("C có giá trị là " + c);
+
+            
+
+        }
+        
+    }
+}
